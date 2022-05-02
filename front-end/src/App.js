d92a7e66aeb22ac2906ec845ee386d8db1fe044b
natferlima <ferlima.natalia@gmail.com>
2022-05-02 16:44:14 -0300

    inicio front

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Routes>
+      <Route exact path="/" element={ <Login /> } />
+      <Route exact path="/login" element={ <Login /> } />
+      <Route exact path="/register" element={ <Register /> } />
+    </Routes>
   );
 }
 
