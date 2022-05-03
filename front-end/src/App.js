90713b22ac0c1f90a0fcc71b46d0a39176d05fd6
Gabriel <gabrieleliasbenedetti@hotmail.com>
2022-05-03 15:42:30 -0300

    Cria a pagina Products e o componente NavBar, ainda sem suas implementacoes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Products from './pages/Products';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route exact path="/" element={ <Login /> } />
       <Route exact path="/login" element={ <Login /> } />
       <Route exact path="/register" element={ <Register /> } />
+      <Route exact path="/customer/products" element= { <Products /> } />
     </Routes>
   );
 }
---
+++ front-end/src/pages/Products.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+function Products() {
+  return( 
+    <NavBar role={}/>
+  ); 
+};
+
+export default Products;
