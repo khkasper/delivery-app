c54584b524f1ff4b8aa82468f810e425dea9417a
natferlima <ferlima.natalia@gmail.com>
2022-05-10 20:43:59 -0300

    refatora pagina de produtos

--- front-end/src/context/CustomerProvider.jsx
+++ front-end/src/context/CustomerProvider.jsx
@@ -1,13 +1,28 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CustomerContext from './CustomerContext';
 
 function CustomerProvider({ children }) {
   const [totalPrice, setTotalPrice] = useState('0.00');
+  const loadCart = () => {
+    if (localStorage.getItem('cart') !== null) {
+      return JSON.parse(localStorage.getItem('cart'));
+    }
+    return [];
+  };
+  const [cart, setCart] = useState(loadCart());
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+    setTotalPrice(cart.reduce((acc, { quantity, price }) => (
+      acc + (quantity * Number(price))
+    ), 0));
+  }, [cart]);
 
   const context = {
     totalPrice,
-    setTotalPrice,
+    cart,
+    setCart,
   };
 
   return (
