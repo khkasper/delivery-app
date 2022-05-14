c71c729e79adbe11b3078be2ae34dd4380bd1587
Jonathan Ferreira <j0n4t@hotmail.com>
2022-05-14 08:10:37 -0300

    refactor: ajusta dependências do useEffect

--- front-end/src/context/AdminProvider.jsx
+++ front-end/src/context/AdminProvider.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useCallback } from 'react';
 import axios from 'axios';
 import AdminContext from './AdminContext';
 import GlobalContext from './GlobalContext';
@@ -8,13 +8,13 @@ function AdminProvider({ children }) {
   const { user: currentUser = {}, setLoading, setError } = useContext(GlobalContext);
   const [users, setUsers] = useState([]);
 
-  const API = axios.create({
-    baseURL: 'http://localhost:3001',
-  });
-
+  const BASE_URL = 'http://localhost:3001';
   const ADMIN_URL = '/admin/manage';
 
-  const loadUsers = async () => {
+  const loadUsers = useCallback(async () => {
+    const API = axios.create({
+      baseURL: BASE_URL,
+    });
     setLoading(true);
     try {
       const { data } = await API.get(
@@ -30,9 +30,12 @@ function AdminProvider({ children }) {
     } catch (err) {
       console.log(err);
     }
-  };
+  }, [currentUser.token, setLoading]);
 
   const registerUserAdmin = async ({ name, email, password, role }) => {
+    const API = axios.create({
+      baseURL: BASE_URL,
+    });
     try {
       await API.post(
         ADMIN_URL,
@@ -45,11 +48,14 @@ function AdminProvider({ children }) {
       );
       await loadUsers();
     } catch (err) {
-      setError(err.response.data);
+      setError(err.response);
     }
   };
 
   const removeUser = async (userId) => {
+    const API = axios.create({
+      baseURL: BASE_URL,
+    });
     setLoading(true);
     try {
       await API.delete(
@@ -69,7 +75,7 @@ function AdminProvider({ children }) {
 
   useEffect(() => {
     if (currentUser.token) loadUsers();
-  }, [currentUser]);
+  }, [currentUser, loadUsers]);
 
   const context = {
     registerUserAdmin,
