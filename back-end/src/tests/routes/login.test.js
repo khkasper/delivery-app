e8a87f40014d885b59960c2ed09e03f21bb3d2cd
Jonathan Ferreira <j0n4t@hotmail.com>
2022-05-26 17:31:46 -0300

    tests(api): primeiro teste em POST `/login`

--- back-end/src/tests/routes/login.test.js
+++ back-end/src/tests/routes/login.test.js
@@ -1,6 +1,20 @@
+const { describe, test } = require('mocha');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+const { expect, request } = chai;
+
+const app = require('../../api/app');
+
 describe('Rota /login', () => {
   describe('Ao passar dados inválidos', () => {
-    test('Retorna erro 400');
+    test('Retorna erro 400', async () => {
+      const response = await request(app)
+        .post('/login');
+      
+      expect(response).to.have.status(400)
+    });
   });
   describe('Ao passar email ou senha inválida', () => {
     test('Retorna erro 404');
