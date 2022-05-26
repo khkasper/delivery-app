4270a01cef5114e6532147b629124855b454261a
Jonathan Ferreira <j0n4t@hotmail.com>
2022-05-26 17:31:12 -0300

    refactor(api): deixa `app` com todas as rotas

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,6 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const { errorHandler, userAuthHandler } = require('../middlewares');
+const {
+  loginRouter,
+  registerRouter,
+  customerRouter,
+  sellerRouter,
+  adminRouter,
+} = require('./routes');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -10,5 +19,13 @@ app.use(cors());
 app.use('/images', express.static('public'));
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
+app.use(userAuthHandler);
+app.use('/customer', customerRouter);
+app.use('/seller', sellerRouter);
+app.use('/admin', adminRouter);
+app.use(errorHandler);
 
 module.exports = app;
--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,21 +1,5 @@
 const port = process.env.PORT || 3001;
 const app = require('./app');
-const { errorHandler, userAuthHandler } = require('../middlewares');
-const {
-  loginRouter,
-  registerRouter,
-  customerRouter,
-  sellerRouter,
-  adminRouter,
-} = require('./routes');
-
-app.use('/login', loginRouter);
-app.use('/register', registerRouter);
-app.use(userAuthHandler);
-app.use('/customer', customerRouter);
-app.use('/seller', sellerRouter);
-app.use('/admin', adminRouter);
-app.use(errorHandler);
 
 app.listen(port);
 console.log(`Api rodando na porta ${port}`);
