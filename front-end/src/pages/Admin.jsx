48ce8ea2a101df5d6583da55931e5b497d30e724
Jonathan Ferreira <j0n4t@hotmail.com>
2022-05-31 00:40:25 -0300

    refactor: `AddUserForm`

--- front-end/src/pages/Admin.jsx
+++ front-end/src/pages/Admin.jsx
@@ -1,33 +1,10 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
+import { Heading, Stack } from '@chakra-ui/react';
 import NavBar from '../components/NavBar';
 import NavItem from '../components/NavItem';
-import Input from '../components/Input';
-import Button from '../components/Button';
-import { registerValidate } from '../utils/validation';
-import GlobalContext from '../context/GlobalContext';
+import AddUserForm from '../components/AddUserForm';
 
 function Admin() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [role, setRole] = useState('seller');
-  const [disabled, setDisabled] = useState(true);
-
-  const { registerUserAdmin, error } = useContext(GlobalContext);
-
-  const registerUser = () => {
-    registerUserAdmin({ name, email, password, role });
-    setName('');
-    setEmail('');
-    setPassword('');
-    setRole('seller');
-  };
-
-  useEffect(() => {
-    const result = registerValidate(name, email, password);
-    setDisabled(result);
-  }, [name, email, password]);
-
   return (
     <main>
       <div>
@@ -39,55 +16,10 @@ function Admin() {
           />
         </NavBar>
       </div>
-      <div>
-        <h2>Cadastrar novo usuário</h2>
-        <span>Nome</span>
-        <Input
-          testId="admin_manage__input-name"
-          type="name"
-          name="name"
-          value={ name }
-          handleChange={ (e) => setName(e.target.value) }
-        />
-        <span>Email</span>
-        <Input
-          testId="admin_manage__input-email"
-          type="email"
-          name="email"
-          value={ email }
-          handleChange={ (e) => setEmail(e.target.value) }
-        />
-        <span>Senha</span>
-        <Input
-          testId="admin_manage__input-password"
-          type="password"
-          name="password"
-          value={ password }
-          handleChange={ (e) => setPassword(e.target.value) }
-        />
-        <span htmlFor="admin_manage__select-role">Tipo</span>
-        <select
-          data-testid="admin_manage__select-role"
-          name="role"
-          value={ role }
-          onChange={ (e) => setRole(e.target.value) }
-        >
-          <option value="seller">Vendedor</option>
-          <option value="customer">Cliente</option>
-          <option value="admin">Administrador</option>
-        </select>
-        <Button
-          testId="admin_manage__button-register"
-          text="CADASTRAR"
-          disabled={ disabled }
-          handleClick={ registerUser }
-        />
-        {error && (
-          <span data-testid="admin_manage__element-invalid-register">
-            {error.message}
-          </span>
-        )}
-      </div>
+      <Stack>
+        <Heading p="5" size="md">Cadastrar novo usuário</Heading>
+        <AddUserForm />
+      </Stack>
     </main>
   );
 }
